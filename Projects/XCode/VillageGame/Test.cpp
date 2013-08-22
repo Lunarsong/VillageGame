4244baf0882cee8c0267fe517223f75a765c1b14
Shanee <shanee.nishry@gmail.com>
2013-08-22 23:11:22 +0300

    Added iOS Project! Let's go! :)

---
+++ Projects/XCode/VillageGame/Test.cpp
@@ -0,0 +1,29 @@
+//
+//  Test.c
+//  VillageGame
+//
+//  Created by Shanee on 8/22/13.
+//  Copyright (c) 2013 LunaEngine. All rights reserved.
+//
+
+#include <stdio.h>
+
+void Start()
+{
+    
+}
+
+void Update( float fDeltaSeconds )
+{
+    
+}
+
+void Render()
+{
+    
+}
+
+void End()
+{
+    
+}
