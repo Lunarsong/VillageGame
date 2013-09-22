1365461b0f48bda1aa40f9135952266ae2ebde45
Lunarsong <shanee.nishry@gmail.com>
2013-09-23 00:03:13 +0300

    merge

--- Source/Source - Copy.cpp
+++ Source/Source - Copy.cpp
@@ -17,7 +17,8 @@ UIElement* pUI;
 #include "AI/Pathfinding/AStar/AStar.h"
 
 SquarePathfindingGraph* pPathGraph = new SquarePathfindingGraph();
-
+PathPlan* pPath = NULL;
+shared_ptr<SpriteComponent> pSprites[30][30];
 class myTouchHandler : public ITouchHandler
 {
 public:
@@ -43,6 +44,60 @@ public:
     bool VOnTouchEnd( const int iTouchIndex, const Vector2& vPosition, const int iPressure )
     {
         --iNumTouchs;
+        
+		if ( iTouchIndex == 0 )
+		{
+			if ( pPath )
+			{
+                pPath->ResetPath();
+                while ( pPath->CheckForEnd() == false )
+                {
+                    const Vector3& vPosition = pPath->GetCurrentNodePosition();
+                    unsigned int uiX = (unsigned int)((vPosition.x) / 32.0f );
+                    unsigned int uiY = (unsigned int)((vPosition.y) / 32.0f );
+                    
+                    ColorF color = ColorF::GREEN;
+                    if ( ( uiX + uiY ) % 2 == 0 )
+                    {
+                        color = ColorF::BLUE;
+                    }
+                    
+                    
+                    pSprites[ uiY ][ uiX ]->SetColor( color );
+                    
+                    pPath->CheckForNextNode( vPosition );
+                }
+                
+                
+				delete pPath;
+			}
+            
+            
+			auto pStartNode = pPathGraph->VFindClosestNode( pEntity->GetTransform().GetPosition() );
+			auto pEndNode = pPathGraph->VFindClosestNode( vPosition );
+			pPath = AStar::FindPath( pPathGraph, pStartNode, pEndNode );
+            
+            
+            
+            if ( pPath )
+            {
+                pPath->ResetPath();
+                while ( pPath->CheckForEnd() == false )
+                {
+                    const Vector3& vPosition = pPath->GetCurrentNodePosition();
+                    unsigned int uiX = (unsigned int)((vPosition.x) / 32.0f );
+                    unsigned int uiY = (unsigned int)((vPosition.y) / 32.0f );
+                    
+                    pSprites[ uiY ][ uiX ]->SetColor( ColorF::GREY );
+                    
+                    pPath->CheckForNextNode( vPosition );
+                }
+                pPath->ResetPath();
+            }
+            
+            
+		}
+		
 
 		return false;
     }
@@ -80,7 +135,6 @@ public:
     }
 };
 
-PathPlan* pPath = NULL;
 class myMouseHandler : public IMouseHandler
 {
 public:
@@ -109,19 +163,78 @@ public:
 	{
 		if ( iButtonIndex == 1 )
 		{
-			pPathGraph->VFindClosestNode( vPosition )->SetBlocked( true );
+			auto pNode = pPathGraph->VFindClosestNode( vPosition );
+            pNode->SetBlocked( pNode->IsTraversable() );
+            
+            unsigned int uiX = (unsigned int)((vPosition.x) / 32.0f );
+            unsigned int uiY = (unsigned int)((vPosition.y) / 32.0f );
+            
+            ColorF color = ColorF::GREEN;
+            if ( ( uiX + uiY ) % 2 == 0 )
+            {
+                color = ColorF::BLUE;
+            }
+            
+            if ( pNode->IsTraversable() == false )
+            {
+                color = ColorF::RED;
+            }
+            
+            pSprites[ uiY ][ uiX ]->SetColor( color );
+
 		}
 
 		else if ( iButtonIndex == 0 )
 		{
 			if ( pPath )
 			{
+                pPath->ResetPath();
+                while ( pPath->CheckForEnd() == false )
+                {
+                    const Vector3& vPosition = pPath->GetCurrentNodePosition();
+                    unsigned int uiX = (unsigned int)((vPosition.x) / 32.0f );
+                    unsigned int uiY = (unsigned int)((vPosition.y) / 32.0f );
+                    
+                    ColorF color = ColorF::GREEN;
+                    if ( ( uiX + uiY ) % 2 == 0 )
+                    {
+                        color = ColorF::BLUE;
+                    }
+
+                    
+                    pSprites[ uiY ][ uiX ]->SetColor( color );
+                    
+                    pPath->CheckForNextNode( vPosition );
+                }
+
+                
 				delete pPath;
 			}
 
-			auto pStartNode = pPathGraph->GetNode( 0, 0 );
+
+			auto pStartNode = pPathGraph->VFindClosestNode( pEntity->GetTransform().GetPosition() );
 			auto pEndNode = pPathGraph->VFindClosestNode( vPosition );
 			pPath = AStar::FindPath( pPathGraph, pStartNode, pEndNode );
+            
+            
+            
+            if ( pPath )
+            {
+                pPath->ResetPath();
+                while ( pPath->CheckForEnd() == false )
+                {
+                    const Vector3& vPosition = pPath->GetCurrentNodePosition();
+                    unsigned int uiX = (unsigned int)((vPosition.x) / 32.0f );
+                    unsigned int uiY = (unsigned int)((vPosition.y) / 32.0f );
+
+                    pSprites[ uiY ][ uiX ]->SetColor( ColorF::GREY );
+                    
+                    pPath->CheckForNextNode( vPosition );
+                }
+                pPath->ResetPath();
+            }
+            
+            
 		}
 		
 
@@ -142,6 +255,7 @@ UILabel* pLabel;
 myMouseHandler* pMouseHandler;
 
 using namespace VillageGame;
+
 void Start()
 {
 	pPathGraph->Create( 30, 30, 32.0f, false );
@@ -189,24 +303,46 @@ void Start()
     
 	pEntity = Game::CreateEntity( matTransform );
     shared_ptr< QuadComponent > pComponent( new QuadComponent() );
-    pComponent->SetTexture( pTexture );
+    //pComponent->SetTexture( pTexture );
     pEntity->AddComponent( pComponent );
     pEntity->Start();
     
     
     matTransform.Identify();
-    matTransform.SetPosition( Vector4( 00.0f, 0.0f ) );
-    pEntity = Game::CreateEntity( matTransform );
-    shared_ptr<SpriteComponent> pSprite( new SpriteComponent() );
-    pSprite->SetTexture( pTexture );
-    //pEntity->AddComponent( pSprite );
-    //pEntity->Start();
+    matTransform.BuildScale( Vector4( 32.0f, 32.0f, 1.0f ) );
+    
+    for ( unsigned int y = 0; y < 30; ++y )
+	{
+		for ( unsigned int x = 0; x < 30; ++x )
+		{
+			ColorF color = ColorF::GREEN;
+			if ( ( x + y ) % 2 == 0 )
+			{
+				color = ColorF::BLUE;
+			}
+            
+			if ( pPathGraph->GetNode( x, y )->IsTraversable() == false )
+			{
+				color = ColorF::RED;
+			}
+            
+            matTransform.SetPosition( Vector4( x * 32.0f, y * 32.0f ) );
+            pEntity = Game::CreateEntity( matTransform );
+            shared_ptr<SpriteComponent> pSprite( new SpriteComponent() );
+            pSprites[y][x] = pSprite;
+            //pSprite->SetTexture( pTexture );
+            pSprite->SetColor( color );
+            pEntity->AddComponent( pSprite );
+            pEntity->Start();
+		}
+    }
     
     matTransform.Identify();
+    matTransform.BuildScale( 0.25f, 0.25f, 1.0f );
     matTransform.SetPosition( Vector4( 100.0f, 100.0f ) );
     pEntity = Game::CreateEntity( matTransform );
-    pSprite.reset( new SpriteComponent() );
-    pSprite->SetTexture( pTexture );
+    shared_ptr<SpriteComponent> pSprite( new SpriteComponent() );
+    pSprite->SetTexture( "Sheep0007.png" );
     pEntity->AddComponent( pSprite );
     pEntity->Start();
     
@@ -238,13 +374,28 @@ void Update( float fDeltaSeconds )
     mat.SetPosition( mat.GetPosition() + Vector4( 1.0f, 1.0f, 0.0f, 0.0f ) * fDeltaSeconds * 5 );
     pEntity->SetTransform( mat );
 	
+    if ( pPath )
+    {
+        if ( pPath->CheckForEnd() == false )
+        {
+            const Vector3& vPosition = pPath->GetCurrentNodePosition();
+            Vector3 vDirection = (vPosition + Vector3( 16.0f, 16.0f ) ) - mat.GetPosition();
+            vDirection.Normalize();
+            mat.SetPosition( mat.GetPosition() + vDirection * fDeltaSeconds * 10 );
+            pEntity->SetTransform( mat );
+            RenderUtils::DrawRectangle( vPosition, Vector2( 32.0f, 32.0f ), ColorF::GREY );
+            
+            pPath->CheckForNextNode( mat.GetPosition() - Vector3( 16.0f, 16.0f ) );
+        }
+    }
+    
 }
 
 void Render()
 {
 	IRenderContext* pRenderContext = IRenderer::Get()->VGetMainContext();
 
-	/*for ( unsigned int y = 0; y < 30; ++y )
+	for ( unsigned int y = 0; y < 30; ++y )
 	{
 		for ( unsigned int x = 0; x < 30; ++x )
 		{
@@ -258,29 +409,18 @@ void Render()
 			{
 				color = ColorF::RED;
 			}
-
-			RenderUtils::DrawRectangle( Vector2( x * 32.0f, y * 32.0f ), Vector2( 32.0f, 32.0f ), color );
+            
 		}
 
-		if ( pPath )
-		{
-			pPath->ResetPath();
-			while ( pPath->CheckForEnd() == false )
-			{
-				const Vector3& vPosition = pPath->GetCurrentNodePosition();
-				RenderUtils::DrawRectangle( vPosition, Vector2( 32.0f, 32.0f ), ColorF::GREY );
-
-				pPath->CheckForNextNode( vPosition );
-			}
-		}
-	}*/
+		
+	}
 
 	IRenderer* pRenderer = IRenderer::Get();
 	Vector3 vScreen( (float)pRenderer->VGetScreenWidth(), (float)pRenderer->VGetScreenHeight(), 1.0f );
 
 	UICoordinates::SetScreen( vScreen, Vector3( 1280.0f, 720.0f, 1.0f ) );
 
-    pUI->Draw( pRenderContext );
+    //pUI->Draw( pRenderContext );
 }
 
 void End()
@@ -289,6 +429,14 @@ void End()
     
     pFont->Release();
     pUI->Release();
+    
+    for ( int x = 0; x < 30; ++x )
+    {
+        for ( int y = 0; y < 30; ++y )
+        {
+            pSprites[y][x] = nullptr;
+        }
+    }
 
 	delete pMouseHandler;
 }
